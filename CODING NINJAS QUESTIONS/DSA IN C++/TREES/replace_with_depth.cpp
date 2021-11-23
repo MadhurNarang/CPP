abe63711a84292931549f1d0237bc3c6d70d9a59
Madhur <madhur.narang.official@gmail.com>
2021-11-23 21:22:43 +0530

    REPLACE DEPTH HALF DONE

---
+++ CODING NINJAS QUESTIONS/DSA IN C++/TREES/replace_with_depth.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include "tree_node.h"
+#include <queue>
+#include <climits>
+
+treenode<int> *inputtree()
+{
+    int rootdata;
+    cout << "ENTER ROOTDATA : ";
+    cin >> rootdata;
+
+    treenode<int> *root = new treenode<int>(rootdata);
+
+    queue<treenode<int> *> pendingnodes;
+
+    pendingnodes.push(root);
+
+    while (pendingnodes.size() != 0)
+    {
+        treenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << "ENTER NUMBER OF CHILDREN  OF " << front->data << " : ";
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int childdata;
+            cout << "ENTER " << i + 1 << "th child of " << front->data << " : ";
+            cin >> childdata;
+
+            treenode<int> *child = new treenode<int>(childdata);
+            front->children.push_back(child);
+
+            pendingnodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+void printtree(treenode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<treenode<int> *> pendingnodes;
+    pendingnodes.push(root);
+
+    while (pendingnodes.size() != 0)
+    {
+        treenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << front->data << " : ";
+
+        for (int i = 0; i < front->children.size(); i++)
+        {
+            cout << front->children.at(i)->data << " , ";
+
+            pendingnodes.push(front->children.at(i));
+        }
+        cout << endl;
+    }
+}
+
+treenode<int> *replacedepth(treenode<int> *root)
+{
+}
+
+int main()
+{
+    treenode<int> *root = inputtree();
+
+    root = replacedepth(root);
+
+    printtree(root);
+
+    return 0;
+}
