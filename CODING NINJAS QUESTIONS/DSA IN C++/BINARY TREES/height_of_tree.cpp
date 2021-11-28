3cede3184ce7321afa7772fe311fe0e945bf7a30
Madhur <madhur.narang.official@gmail.com>
2021-11-28 16:19:50 +0530

    HEIGHT OF BINARY TREE DONE

---
+++ CODING NINJAS QUESTIONS/DSA IN C++/BINARY TREES/height_of_tree.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+#include "binary_tree_node.h"
+
+void printbinarytree(binarytreenode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<binarytreenode<int> *> pendingnodes;
+
+    pendingnodes.push(root);
+
+    while (!pendingnodes.empty())
+    {
+        binarytreenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << front->data << " :";
+
+        if (front->left) //(root->left!=NULL)
+        {
+            cout << " L" << front->left->data;
+            pendingnodes.push(front->left);
+        }
+        if (front->right) //(root->right!=NULL)
+        {
+            cout << " R" << front->right->data;
+            pendingnodes.push(front->right);
+        }
+        cout << endl;
+    }
+}
+
+binarytreenode<int> *inputbinarytree()
+{
+    int rootdata;
+    cout << "ENTER ROOT DATA : ";
+    cin >> rootdata;
+
+    if (rootdata == -1)
+    {
+        return NULL;
+    }
+
+    binarytreenode<int> *root = new binarytreenode<int>(rootdata);
+
+    queue<binarytreenode<int> *> pendingnodes;
+
+    pendingnodes.push(root);
+
+    while (!pendingnodes.empty())
+    {
+        binarytreenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << "ENTER LEFT CHILD OF " << front->data << " : ";
+        int leftchilddata;
+        cin >> leftchilddata;
+        if (leftchilddata != -1)
+        {
+            binarytreenode<int> *leftchild = new binarytreenode<int>(leftchilddata);
+            pendingnodes.push(leftchild);
+            front->left = leftchild;
+        }
+
+        cout << "ENTER RIGHT CHILD OF " << front->data << " : ";
+        int rightchilddata;
+        cin >> rightchilddata;
+        if (rightchilddata != -1)
+        {
+            binarytreenode<int> *rightchild = new binarytreenode<int>(rightchilddata);
+            pendingnodes.push(rightchild);
+            front->right = rightchild;
+        }
+    }
+
+    return root;
+}
+
+int treeheight(binarytreenode<int> *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int smallheight1 = treeheight(root->left);
+    int smallheight2 = treeheight(root->right);
+
+    if (smallheight1 > smallheight2)
+    {
+        return smallheight1 + 1;
+    }
+    else
+    {
+        return smallheight2 + 1;
+    }
+}
+
+int main()
+{
+    binarytreenode<int> *root = inputbinarytree();
+
+    cout << treeheight(root) << endl;
+
+    delete root;
+}
---
+++ CODING NINJAS QUESTIONS/DSA IN C++/BINARY TREES/is_node_present.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+#include "binary_tree_node.h"
+
+void printbinarytree(binarytreenode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<binarytreenode<int> *> pendingnodes;
+
+    pendingnodes.push(root);
+
+    while (!pendingnodes.empty())
+    {
+        binarytreenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << front->data << " :";
+
+        if (front->left) //(root->left!=NULL)
+        {
+            cout << " L" << front->left->data;
+            pendingnodes.push(front->left);
+        }
+        if (front->right) //(root->right!=NULL)
+        {
+            cout << " R" << front->right->data;
+            pendingnodes.push(front->right);
+        }
+        cout << endl;
+    }
+}
+
+binarytreenode<int> *inputbinarytree()
+{
+    int rootdata;
+    cout << "ENTER ROOT DATA : ";
+    cin >> rootdata;
+
+    if (rootdata == -1)
+    {
+        return NULL;
+    }
+
+    binarytreenode<int> *root = new binarytreenode<int>(rootdata);
+
+    queue<binarytreenode<int> *> pendingnodes;
+
+    pendingnodes.push(root);
+
+    while (!pendingnodes.empty())
+    {
+        binarytreenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << "ENTER LEFT CHILD OF " << front->data << " : ";
+        int leftchilddata;
+        cin >> leftchilddata;
+        if (leftchilddata != -1)
+        {
+            binarytreenode<int> *leftchild = new binarytreenode<int>(leftchilddata);
+            pendingnodes.push(leftchild);
+            front->left = leftchild;
+        }
+
+        cout << "ENTER RIGHT CHILD OF " << front->data << " : ";
+        int rightchilddata;
+        cin >> rightchilddata;
+        if (rightchilddata != -1)
+        {
+            binarytreenode<int> *rightchild = new binarytreenode<int>(rightchilddata);
+            pendingnodes.push(rightchild);
+            front->right = rightchild;
+        }
+    }
+
+    return root;
+}
+
+bool checknode(binarytreenode<int> *root, int x)
+{
+    bool ans = false;
+
+    if (root == NULL)
+    {
+        return ans;
+    }
+
+    if (root->data == x)
+    {
+        ans = true;
+        return ans;
+    }
+
+    return (ans || checknode(root->left, x) || checknode(root->right, x));
+}
+
+int main()
+{
+    binarytreenode<int> *root = inputbinarytree();
+
+    printbinarytree(root);
+    cout << endl;
+
+    int x;
+    cin >> x;
+    cout << checknode(root, x) << endl;
+
+    delete root;
+}
