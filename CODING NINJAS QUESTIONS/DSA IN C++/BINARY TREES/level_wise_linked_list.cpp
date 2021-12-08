d7d824563c57c6b6a6073de82bdf99b706565aeb
Madhur <madhur.narang.official@gmail.com>
2021-12-08 14:08:15 +0530

    LEVELWISE LINKED LIST HALF DONE

---
+++ CODING NINJAS QUESTIONS/DSA IN C++/BINARY TREES/level_wise_linked_list.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+#include "binary_tree_node.h"
+
+void printbinarytree(binarytreenode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<binarytreenode<int> *> pendingnodes;
+
+    pendingnodes.push(root);
+
+    while (!pendingnodes.empty())
+    {
+        binarytreenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << front->data << " :";
+
+        if (front->left) //(root->left!=NULL)
+        {
+            cout << " L" << front->left->data;
+            pendingnodes.push(front->left);
+        }
+        if (front->right) //(root->right!=NULL)
+        {
+            cout << " R" << front->right->data;
+            pendingnodes.push(front->right);
+        }
+        cout << endl;
+    }
+}
+
+binarytreenode<int> *inputbinarytree()
+{
+    int rootdata;
+    cout << "ENTER ROOT DATA : ";
+    cin >> rootdata;
+
+    if (rootdata == -1)
+    {
+        return NULL;
+    }
+
+    binarytreenode<int> *root = new binarytreenode<int>(rootdata);
+
+    queue<binarytreenode<int> *> pendingnodes;
+
+    pendingnodes.push(root);
+
+    while (!pendingnodes.empty())
+    {
+        binarytreenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << "ENTER LEFT CHILD OF " << front->data << " : ";
+        int leftchilddata;
+        cin >> leftchilddata;
+        if (leftchilddata != -1)
+        {
+            binarytreenode<int> *leftchild = new binarytreenode<int>(leftchilddata);
+            pendingnodes.push(leftchild);
+            front->left = leftchild;
+        }
+
+        cout << "ENTER RIGHT CHILD OF " << front->data << " : ";
+        int rightchilddata;
+        cin >> rightchilddata;
+        if (rightchilddata != -1)
+        {
+            binarytreenode<int> *rightchild = new binarytreenode<int>(rightchilddata);
+            pendingnodes.push(rightchild);
+            front->right = rightchild;
+        }
+    }
+
+    return root;
+}
+
+class node
+{
+public:
+    binarytreenode<int> *data;
+    node *next;
+
+    node()
+    {
+        this->data = NULL;
+        next = NULL;
+    }
+
+    node(binarytreenode<int> *data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+pair<vector<node *>, int> levelwiselinkedlist(binarytreenode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<binarytreenode<int> *> pendingnodes;
+    vector<node *> v;
+    node *head = new node;
+    node *temp = new node;
+
+    pendingnodes.push(root);
+    pendingnodes.push(NULL);
+
+    head->data = root;
+
+    while (!pendingnodes.empty())
+    {
+        binarytreenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        if (front == NULL)
+        {
+            cout << endl;
+            temp->next = new node(front);
+            v.push_back(head);
+            head->data = pendingnodes.front();
+            temp = head;
+
+            if (pendingnodes.empty())
+            {
+                head->data = null;
+                break;
+            }
+            else
+                pendingnodes.push(NULL);
+        }
+        else
+        {
+            cout << front->data << " ";
+
+            if (front->left) //(root->left!=NULL)
+            {
+                pendingnodes.push(front->left);
+            }
+            if (front->right) //(root->right!=NULL)
+            {
+                pendingnodes.push(front->right);
+            }
+
+            temp->data = front;
+            temp = temp->next;
+        }
+    }
+}
+
+void printlinkedlist(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    binarytreenode<int> *root = inputbinarytree();
+
+    pair<vector<node *>, int> ans = levelwiselinkedlist(root);
+
+    for (int i = 0; i < ans.second; i++)
+    {
+        printlinkedlist(ans.first[i]);
+        cout << endl;
+    }
+
+    delete root;
+}
