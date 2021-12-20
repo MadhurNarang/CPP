b6f35f0a97c31e82dcbf373751cf7f1dfb437ecc
dellMadhur <madhur.narang.official@gmail.com>
2021-12-20 16:48:11 +0530

    ROOT TO LEAF PATH HALD DONE

---
+++ CODING NINJAS QUESTIONS/DSA IN C++/BST/path_sum_root_to_leaf_in_binary_tree.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <climits>
+using namespace std;
+#include "binary_tree_node.h"
+
+void printbinarytree(binarytreenode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<binarytreenode<int> *> pendingnodes;
+
+    pendingnodes.push(root);
+
+    while (!pendingnodes.empty())
+    {
+        binarytreenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << front->data << " :";
+
+        if (front->left) //(root->left!=NULL)
+        {
+            cout << " L" << front->left->data;
+            pendingnodes.push(front->left);
+        }
+        if (front->right) //(root->right!=NULL)
+        {
+            cout << " R" << front->right->data;
+            pendingnodes.push(front->right);
+        }
+        cout << endl;
+    }
+}
+
+binarytreenode<int> *inputbinarytree()
+{
+    int rootdata;
+    cout << "ENTER ROOT DATA : ";
+    cin >> rootdata;
+
+    if (rootdata == -1)
+    {
+        return NULL;
+    }
+
+    binarytreenode<int> *root = new binarytreenode<int>(rootdata);
+
+    queue<binarytreenode<int> *> pendingnodes;
+
+    pendingnodes.push(root);
+
+    while (!pendingnodes.empty())
+    {
+        binarytreenode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+
+        cout << "ENTER LEFT CHILD OF " << front->data << " : ";
+        int leftchilddata;
+        cin >> leftchilddata;
+        if (leftchilddata != -1)
+        {
+            binarytreenode<int> *leftchild = new binarytreenode<int>(leftchilddata);
+            pendingnodes.push(leftchild);
+            front->left = leftchild;
+        }
+
+        cout << "ENTER RIGHT CHILD OF " << front->data << " : ";
+        int rightchilddata;
+        cin >> rightchilddata;
+        if (rightchilddata != -1)
+        {
+            binarytreenode<int> *rightchild = new binarytreenode<int>(rightchilddata);
+            pendingnodes.push(rightchild);
+            front->right = rightchild;
+        }
+    }
+
+    return root;
+}
+
+void path_root_to_leaf(binarytreenode<int> *root, int k)
+{
+}
+
+int main()
+{
+    binarytreenode<int> *root = inputbinarytree();
+    int k;
+    cin >> k;
+
+    path_root_to_leaf(root, k);
+
+    printbinarytree(root);
+
+    delete root;
+}
