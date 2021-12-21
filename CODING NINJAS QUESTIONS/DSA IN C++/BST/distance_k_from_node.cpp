44345f1f734411261149f155f11e2914fbc0d709
dellMadhur <madhur.narang.official@gmail.com>
2021-12-21 12:31:10 +0530

    DISTANCE K FROM NODE METHOD 2 DONE AND METHOD 1 NOT DONE

--- CODING NINJAS QUESTIONS/DSA IN C++/BST/distance_k_from_node.cpp
+++ CODING NINJAS QUESTIONS/DSA IN C++/BST/distance_k_from_node.cpp
@@ -84,18 +84,87 @@ binarytreenode<int> *inputbinarytree()
     return root;
 }
 
-void distance_from_node(binarytreenode<int> * root,int node,int k)
+void print_nodes_at_depth(binarytreenode<int> *root, int k)
 {
-    
+    if (root == NULL)
+    {
+        return;
+    }
+
+    if (k == 0)
+    {
+        cout << root->data << " ";
+        return;
+    }
+    else
+    {
+        print_nodes_at_depth(root->left, k - 1);
+        print_nodes_at_depth(root->right, k - 1);
+    }
+}
+
+int distance_from_node(binarytreenode<int> *root, int node, int k)
+{
+    if (root == NULL)
+    {
+        return -1;
+    }
+
+    if (root->data == node)
+    {
+        print_nodes_at_depth(root, k);
+        return 0;
+    }
+    else
+    {
+        int ld = distance_from_node(root->left, node, k);
+        if (ld != -1)
+        {
+            if (ld + 1 == k)
+            {
+                cout << root->data << " ";
+            }
+            else
+            {
+                print_nodes_at_depth(root->right, k - ld - 2);
+            }
+        }
+
+        int rd = distance_from_node(root->right, node, k);
+        if (rd != -1)
+        {
+            if (rd + 1 == k)
+            {
+                cout << root->data;
+            }
+            else
+            {
+                print_nodes_at_depth(root->left, k - rd - 2);
+            }
+        }
+
+        if (ld == -1 && rd == -1)
+        {
+            return -1;
+        }
+        else if (ld != -1)
+        {
+            return ld + 1;
+        }
+        else
+        {
+            return rd + 1;
+        }
+    }
 }
 
 int main()
 {
     binarytreenode<int> *root = inputbinarytree();
-    int node,k;
-    cin >>node >>k;
+    int node, k;
+    cin >> node >> k;
 
-    distance_from_node(root, node,k);
+    distance_from_node(root, node, k);
 
     delete root;
 }
